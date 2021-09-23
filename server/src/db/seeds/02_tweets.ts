8c256dc0f65729be12049bd01fb89713c95ae36d
RealPenguin <mrjanggo858@gmail.com>
2021-09-23 18:35:13 +0700

    deleted usepost and change the post method to the form itself

--- server/src/db/seeds/02_tweets.ts
+++ server/src/db/seeds/02_tweets.ts
@@ -1,14 +1,10 @@
 import { Knex } from "knex";
-import bcrypt from 'bcrypt'
-
-var password = bcrypt.hash('admin', 10);
 
 export async function seed(knex: Knex): Promise<void> {
     await knex("table_name").del();
     await knex("table_name").insert([
         { 
-           username: 'Admin',
-           password: password
+            colName: "rowValue1" 
         },
     ]);
 };
--- web/components/RegisterForm.tsx
+++ web/components/RegisterForm.tsx
@@ -3,35 +3,86 @@ import {
     TextField,
     Button
 } from "@mui/material";
+import { useState } from "react";
 import useForm from "../hooks/useForm";
-import usePost from "../hooks/usePost";
 
 const RegisterForm = () => {
     const [data, setData] = useForm({
         username: '',
         password: ''
     })
+    const [error, setError] = useState<any>({
+        usernameError: '',
+        isError: false,
+        passwordError: ''
+    })
+    
+    const validate = () => {
+        if(!data.username || !data.password) {
+            setError({
+                usernameError: 'Username cant be empty',
+                isError: true,
+                passwordError: 'Password cant be empty'
+            })
+            return false
+        }
+        return true
+    }
+
     const handleSubmit = (e: any) => {
         e.preventDefault();
-        usePost(`${process.env.BASE_URL}/auth/register`, {
-            username: data.username,
-            password: data.password
-        }).then((res) => {
-            console.log(res);
-        })
-        .catch((err) => {
-            console.error(err)
-        })
+        const isValid = validate();
+        if(isValid) {
+           fetch(`${process.env.BASE_URL}/auth/register`, {
+               method: 'POST',
+               headers: {
+                    'Content-Type': 'application/json',
+               },
+               mode: 'cors',
+               body: JSON.stringify({
+                   username: data.username,
+                   password: data.password
+               })
+           })
+           .then((res) => {
+                res.json()
+           })
+           .then((data) => {
+               console.log(data);
+           })
+           .catch((err) => {
+               console.error(err)
+           })
+        }
     }
     return(
         <form onSubmit={handleSubmit}>
+            <h3 style={{ marginBottom: '1rem' }}>Register</h3>
+            <div style={{ fontSize: '12px', color: 'red' }}>
+                    {error.usernameError}
+            </div>
             <TextField 
-            id="outlined-basic" 
-            name="username"
-            value={data.username}
-            onChange={setData}
-            label="Username" 
-            variant="outlined" 
+                id="outlined-basic" 
+                name="username"
+                type="text"
+                value={data.username}
+                onChange={setData}
+                label="Username" 
+                variant="outlined" 
+                style={{ marginBottom: '1rem' }}
+            />
+             <div style={{ fontSize: '12px', color: 'red' }}>
+                    {error.passwordError}
+            </div>
+            <TextField 
+                id="outlined-basic" 
+                name="password"
+                type="password"
+                value={data.password}
+                onChange={setData}
+                label="Password" 
+                variant="outlined" 
+                style={{ marginBottom: '1rem' }}
             />
             <Button variant="outlined" type="submit">Register</Button>
         </form>
