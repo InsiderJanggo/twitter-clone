5c98407f8f1fee44c430518887ca5f533828179a
RealPenguin <mrjanggo858@gmail.com>
2021-09-23 19:15:33 +0700

    updated loginform component

--- web/components/LoginForm.tsx
+++ web/components/LoginForm.tsx
@@ -4,3 +4,62 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import useForm from "../hooks/useForm";
+
+const LoginForm = () => {
+    const [data, setData] = useForm({
+        username: '',
+        password: ''
+    })
+    const [error, setError] = useState<any>({
+        usernameError: '',
+        isError: false,
+        passwordError: ''
+    })
+
+    const validate = () => {
+        if(!data.username || !data.password) {
+            setError({
+                usernameError: 'Username cant be empty',
+                isError: true,
+                passwordError: 'Password cant be empty'
+            })
+            return false
+        }
+        return true
+    }
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+        const isValid = validate();
+        if(isValid) {
+           fetch(`${process.env.BASE_URL}/auth/login`, {
+               method: 'POST',
+               headers: {
+                    'Content-Type': 'application/json',
+               },
+               mode: 'cors',
+               body: JSON.stringify({
+                   username: data.username,
+                   password: data.password
+               })
+           })
+           .then((res) => {
+                res.json()
+           })
+           .then((data) => {
+              localStorage.setItem('user', JSON.stringify(data))
+           })
+           .catch((err) => {
+               console.error(err)
+           })
+        }
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+
+        </form>
+    )
+}
+
+export default LoginForm
